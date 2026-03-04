0091c6cbd5e91653298b2135e8b77e43ac07a730
Sofian-bll <Sofianebelloul@gmail.com>
2026-03-04 13:50:02 +0100

    feat(api-comments): add comments model, migration, and relationships
    - Create `Comment` model with relationships to `Post` and `User`
    - Add migration for `comments` table with foreign key constraints
    - Update `Post` and `User` models to include `comments` relationship
    
    details:features/backend-api-comments
    Signed-off-by: Sofian-bll <Sofianebelloul@gmail.com>

--- connect-in-api/app/Models/Post.php
+++ connect-in-api/app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  * @property mixed $author_id
@@ -19,4 +20,10 @@ class Post extends Model
     {
         return $this->belongsTo(User::class, 'author_id');
     }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+
 }

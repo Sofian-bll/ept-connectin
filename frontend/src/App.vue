879708dde3690c0b279152961d106b1f79c5f3c5
Sofian-bll <Sofianebelloul@gmail.com>
2026-03-07 01:21:58 +0100

    feat(frontend): integrate TailwindCSS and enhance example component
    - Configure `tailwindcss` with Vite and install required dependencies
    - Update `jsconfig.json` to include `baseUrl` for simplified imports
    - Enhance `App.vue` with a structured sample post object for demo purposes
    - Add `style.css` with TailwindCSS import for global styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,36 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div>
+    <p>{{post.data.id}}</p>
+  </div>
 </template>
 
-<style scoped></style>
+<script setup>
+
+const post = {
+  "data": {
+    "id": 2,
+    "title": "Commodi illum fuga hic.",
+    "content": "Numquam amet optio illum nihil aut totam sunt aut. Ipsa unde odit aut perspiciatis eum eos. Vel magnam aut sit incidunt in omnis consectetur.\n\nCorrupti repellat inventore quia qui aliquid. Id odio voluptate est iusto. Non non sunt beatae dolore aut doloremque commodi.\n\nAut neque repellat iure nesciunt. Est labore libero et nemo sint ut. Saepe mollitia quos doloremque dolore illo occaecati et.",
+    "media_url": "/storage/media/M2tGLHBF8lx5EnOXP9zC55fQ2TPRHUvfrKk6hKLH.jpg",
+    "author": {
+      "id": 5,
+      "first_name": "Mylene",
+      "last_name": "Mitchell",
+      "name": "Mylene Mitchell",
+      "email": "ramona16@example.org",
+      "avatar": "/storage/avatars/5.jpg",
+      "birthday": "1989-05-03",
+      "birthplace_city": null,
+      "birthplace_country": null,
+      "bio": "SAID was, 'Why is a long time with the words 'EAT ME' were beautifully marked in currants. 'Well, I'll eat it,' said Alice. 'That's very important,' the King said, turning to Alice as she could, and.",
+      "created_at": "2026-03-06T22:59:13.000000Z",
+      "updated_at": "2026-03-06T22:59:13.000000Z"
+    },
+    "likes_count": 0,
+    "is_liked": false,
+    "created_at": "2026-03-06 22:59:13",
+    "updated_at": "2026-03-06 22:59:13"
+  }
+}
+
+</script>
